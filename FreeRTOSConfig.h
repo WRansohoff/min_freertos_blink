1bf49dc73bb85ff6fb92f4596b951cb299dbc2ca
Will Ransohoff <wransohoff@gmail.com>
2018-08-22 19:40:40 -0700

    Update FreeRTOSConfig.h to use a v10.0.1 template

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -28,58 +28,107 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-/*-----------------------------------------------------------
- * Application specific definitions.
- *
- * These definitions should be adjusted for your particular hardware and
- * application requirements.
- *
- * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE. 
- *
- * See http://www.freertos.org/a00110.html.
- *----------------------------------------------------------*/
-
-#define configUSE_PREEMPTION            1
-#define configUSE_IDLE_HOOK             0
-#define configUSE_TICK_HOOK             0
-#ifdef VVC_F1
+#define configUSE_PREEMPTION                    1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
+#define configUSE_TICKLESS_IDLE                 0
+#ifdef VVC_F0
+  #define configCPU_CLOCK_HZ              ( ( unsigned long ) 48000000 )  
+  #define configSYSTICK_CLOCK_HZ          ( configCPU_CLOCK_HZ / 8 )
+#elif  VVC_F1
   #define configCPU_CLOCK_HZ              ( ( unsigned long ) 72000000 )  
   #define configSYSTICK_CLOCK_HZ          ( configCPU_CLOCK_HZ / 8 )
-#else
+#elif  VVC_F3
   #define configCPU_CLOCK_HZ              ( ( unsigned long ) 8000000 )  
   #define configSYSTICK_CLOCK_HZ          ( configCPU_CLOCK_HZ / 8 )
+#elif  VVC_L0
+  #define configCPU_CLOCK_HZ              ( ( unsigned long ) 32000000 )  
+  #define configSYSTICK_CLOCK_HZ          ( configCPU_CLOCK_HZ / 8 )
 #endif
-#define configTICK_RATE_HZ              ( ( TickType_t ) 250 )
-#define configMAX_PRIORITIES            ( 5 )
-#define configMINIMAL_STACK_SIZE        ( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 8 * 1024 ) )
-#define configMAX_TASK_NAME_LEN         ( 16 )
-#define configUSE_TRACE_FACILITY        0
-#define configUSE_16_BIT_TICKS          0
-#define configIDLE_SHOULD_YIELD         1
-#define configUSE_MUTEXES               0
-#define configCHECK_FOR_STACK_OVERFLOW  0
+#define configTICK_RATE_HZ                      250
+#define configMAX_PRIORITIES                    5
+#define configMINIMAL_STACK_SIZE                128
+#define configMAX_TASK_NAME_LEN                 16
+#define configUSE_16_BIT_TICKS                  0
+#define configIDLE_SHOULD_YIELD                 1
+#define configUSE_TASK_NOTIFICATIONS            0
+#define configUSE_MUTEXES                       0
+#define configUSE_RECURSIVE_MUTEXES             0
+#define configUSE_COUNTING_SEMAPHORES           0
+#define configQUEUE_REGISTRY_SIZE               10
+#define configUSE_QUEUE_SETS                    0
+#define configUSE_TIME_SLICING                  0
+#define configUSE_NEWLIB_REENTRANT              0
+#define configENABLE_BACKWARD_COMPATIBILITY     0
+#define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
 
-/* Co-routine definitions. */
-#define configUSE_CO_ROUTINES           0
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+/* Memory allocation related definitions. */
+#define configSUPPORT_STATIC_ALLOCATION         0
+#define configSUPPORT_DYNAMIC_ALLOCATION        1
+#if defined(VVC_F0) || defined(VVC_L0)
+  #define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 2 * 1024 ) )
+#else
+  #define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 8 * 1024 ) )
+#endif
+#define configAPPLICATION_ALLOCATED_HEAP        0
 
-/* Set the following definitions to 1 to include the API function, or zero
-to exclude the API function. */
+/* Hook function related definitions. */
+#define configUSE_IDLE_HOOK                     0
+#define configUSE_TICK_HOOK                     0
+#define configCHECK_FOR_STACK_OVERFLOW          0
+#define configUSE_MALLOC_FAILED_HOOK            0
+#define configUSE_DAEMON_TASK_STARTUP_HOOK      0
 
-#define INCLUDE_vTaskPrioritySet       0
-#define INCLUDE_uxTaskPriorityGet      0
-#define INCLUDE_vTaskDelete            0
-#define INCLUDE_vTaskCleanUpResources  0
-#define INCLUDE_vTaskSuspend           0
-#define INCLUDE_vTaskDelayUntil        0
-#define INCLUDE_vTaskDelay             1
+/* Run time and task stats gathering related definitions. */
+#define configGENERATE_RUN_TIME_STATS           0
+#define configUSE_TRACE_FACILITY                0
+#define configUSE_STATS_FORMATTING_FUNCTIONS    0
 
-#define configKERNEL_INTERRUPT_PRIORITY         255
+/* Co-routine related definitions. */
+#define configUSE_CO_ROUTINES                   0
+#define configMAX_CO_ROUTINE_PRIORITIES         1
+
+/* Software timer related definitions. */
+#define configUSE_TIMERS                        0
+#define configTIMER_TASK_PRIORITY               3
+#define configTIMER_QUEUE_LENGTH                10
+#define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
+
+/* Interrupt nesting behaviour configuration. */
+/* Cortex-M specific definitions. */
+#ifdef __NVIC_PRIO_BITS
+  #define configPRIO_BITS         __NVIC_PRIO_BITS
+#else
+  #define configPRIO_BITS         4
+#endif
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY      15
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+#define configKERNEL_INTERRUPT_PRIORITY   ( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY    191
-#define configLIBRARY_KERNEL_INTERRUPT_PRIORITY 15
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY  ( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
+
+/* FreeRTOS MPU specific definitions. */
+#define configINCLUDE_APPLICATION_DEFINED_PRIVILEGED_FUNCTIONS 0
+
+/* Optional functions - most linkers will remove unused functions anyway. */
+#define INCLUDE_vTaskPrioritySet                0
+#define INCLUDE_uxTaskPriorityGet               0
+#define INCLUDE_vTaskDelete                     0
+#define INCLUDE_vTaskSuspend                    0
+#define INCLUDE_xResumeFromISR                  0
+#define INCLUDE_vTaskDelayUntil                 0
+#define INCLUDE_vTaskDelay                      1
+#define INCLUDE_xTaskGetSchedulerState          0
+#define INCLUDE_xTaskGetCurrentTaskHandle       0
+#define INCLUDE_uxTaskGetStackHighWaterMark     0
+#define INCLUDE_xTaskGetIdleTaskHandle          0
+#define INCLUDE_eTaskGetState                   0
+#define INCLUDE_xEventGroupSetBitFromISR        0
+#define INCLUDE_xTimerPendFunctionCall          0
+#define INCLUDE_xTaskAbortDelay                 0
+#define INCLUDE_xTaskGetHandle                  0
+#define INCLUDE_xTaskResumeFromISR              0
+
+/* A header file that defines trace macro can be included here. */
 
 #endif /* FREERTOS_CONFIG_H */
